2a016f18ac8d5c38b748c7098df5ba360edaa4f2
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-03-24 08:09:27 -0300

    added style on prevpokeom

--- components/ui/buttonsNavigation.tsx
+++ components/ui/buttonsNavigation.tsx
@@ -1,17 +1,18 @@
 'use client';
-
 import { useRouter } from 'next/navigation';
 
 export function ButtonNext({ id }: { id: number }) {
   const router = useRouter();
 
   const handleNextPokemon = () => {
-    const nextId = +id + 1; //Aca habia un bug tremendo
+    const nextId = +id + 1;
     router.push(`/pokemon/${nextId}`);
   };
 
   return (
-    <button onClick={handleNextPokemon}>Next Pokemon</button>
+    <button onClick={handleNextPokemon} className="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-full fixed top-1/5 right-[10%] mr-2 z-50">
+      Next Pokemon
+    </button>
   );
 }
 
@@ -19,11 +20,13 @@ export function ButtonPrev({ id }: { id: number }) {
   const router = useRouter();
 
   const handlePrevPokemon = () => {
-    const prevId = +id - 1; 
+    const prevId = +id - 1;
     router.push(`/pokemon/${prevId}`);
   };
 
   return (
-    <button onClick={handlePrevPokemon}>Previous Pokemon</button>
+    <button onClick={handlePrevPokemon} className="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-full fixed top-1/5 left-[10%] ml-2 z-50">
+      Previous Pokemon
+    </button>
   );
 }
