c683ad92d9874807f81d8db40c0793c30c96cbeb
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-03-25 09:13:25 -0300

    Loading skeleton

--- components/pokemonCard.tsx
+++ components/pokemonCard.tsx
@@ -40,7 +40,37 @@ export function PokemonCard({ id, name }: PokemonCardProps) {
   }, [id]);
 
   if (!pokemonData) {
-    return <div>Loading...</div>;
+    const formattedId = id.toString().padStart(4, "0");
+    return (
+      <li className="group rounded-lg overflow-hidden transition-transform ease-in-out duration-200 hover:-translate-y-1 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div className="block rounded-lg m-3 p-5 transition-transform ease-in-out duration-200 hover:-translate-y-1 focus:outline-none focus:ring-4 focus:ring-black focus:ring-opacity-100">
+          <div className="animate-pulse flex flex-col">
+            <div className="bg-gray-200 rounded-md aspect-square mb-4"></div>
+            <div className="flex-1 space-y-4 py-1">
+              <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+              <div className="space-y-2">
+                <div className="h-4 bg-gray-200 rounded"></div>
+                <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+              </div>
+            </div>
+          </div>
+          <span className="text-sm m-2 font-bold text-gray-400 font-flexo-bold mx-4">
+            N.° {formattedId}
+          </span>
+          <h2 className="text-xl font-semibold text-gray-700 font-flexo-demi mt-3 mx-4">
+            {name.charAt(0).toUpperCase() + name.slice(1)}
+          </h2>
+          <div className="flex gap-1 mx-4">
+            {Array.from({ length: 2 }).map((_, index) => (
+              <div
+                key={index}
+                className="inline-block text-center px-3 text-xs rounded-sm font-flexo-medium bg-gray-200 w-[80px] h-6"
+              ></div>
+            ))}
+          </div>
+        </div>
+      </li>
+    );
   }
 
   const { sprite, types } = pokemonData;
