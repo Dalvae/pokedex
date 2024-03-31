8fb418464c49db093d909be72dc4ce56fe91d4f9
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-03-31 19:03:57 -0300

    More accuarate pokemon container

--- components/PokemonContainer.tsx
+++ components/PokemonContainer.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import PokeballPixel from '@/public/icons/pokeballpixel';
+import React from "react";
+import PokeballPixel from "@/public/icons/pokeballpixel";
 
 interface PokemonContainerProps {
   children: React.ReactNode;
 }
 
-
 export function PokemonContainer({ children }: PokemonContainerProps) {
-    return (
-      <div className="max-w-5xl mt-5 rounded-xl mx-[10%] bg-white flex-start min-h-[70vh] justify-center relative border-black border-8 border-double">
-        <div className="absolute top-0 left-0 -mt-4 -ml-4">
+  return (
+    <div className="max-w-5xl mt-5 mx-[10%] bg-white flex-start min-h-[70vh] justify-center relative border-black border-b-[6px] border-2">
+      <div className="m-[3px] bg-white border-black border-2 border-t-[6px]">
+        <div className="absolute top-0 left-0 -mt-2 -ml-2">
           <PokeballPixel className="w-6 h-6" />
         </div>
-        <div className="absolute top-0 right-0 -mt-4 -mr-4">
+        <div className="absolute top-0 right-0 -mt-2 -mr-2">
           <PokeballPixel className="w-6 h-6" />
         </div>
-        <div className="absolute bottom-0 left-0 -mb-4 -ml-4">
+        <div className="absolute bottom-0 left-0 -mb-2 -ml-2">
           <PokeballPixel className="w-6 h-6" />
         </div>
-        <div className="absolute bottom-0 right-0 -mb-4 -mr-4">
+        <div className="absolute bottom-0 right-0 -mb-2 -mr-2">
           <PokeballPixel className="w-6 h-6" />
         </div>
         {children}
       </div>
-    );
-  }
+    </div>
+  );
+}
