0726219dbb7a670fbaeb2326e1b14f0cdffb110b
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-03-31 22:26:06 -0300

    added props on container and old style

--- components/PokemonContainer.tsx
+++ components/PokemonContainer.tsx
@@ -3,12 +3,18 @@ import PokeballPixel from "@/public/icons/pokeballpixel";
 
 interface PokemonContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function PokemonContainer({ children }: PokemonContainerProps) {
+export function PokemonContainer({
+  children,
+  className = "",
+}: PokemonContainerProps) {
   return (
-    <div className="max-w-5xl mt-5 mx-[10%] bg-white flex-start min-h-[70vh] justify-center relative border-black border-b-[6px] border-2">
-      <div className="m-[3px] bg-white border-black border-2 border-t-[6px]">
+    <div
+      className={`max-w-5xl mt-5 mx-[10%] bg-white flex-start min-h-[70vh] justify-center relative border-black border-b-[6px] border-2 ${className}`}
+    >
+      <div className="m-[3px] bg-white border-black border-2 min-h-[70vh] border-t-[6px]">
         <div className="absolute top-0 left-0 -mt-2 -ml-2">
           <PokeballPixel className="w-6 h-6" />
         </div>
